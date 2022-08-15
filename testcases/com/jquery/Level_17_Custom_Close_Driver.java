2f2112381a034f648dea5d047f007033915ce074
Anh Nguyen <xuananh194@gmail.com>
2022-08-15 12:06:34 +0700

    [commit]

--- testcases/com/jquery/Level_17_Custom_Close_Driver.java
+++ testcases/com/jquery/Level_17_Custom_Close_Driver.java
@@ -96,6 +96,7 @@ public class Level_17_Custom_Close_Driver extends BaseTest {
 		//driver.quit();
 		// custom close browser để lúc nào cũng tắt browser driver đi ( dù có gặp bất kỳ vấn đề gì đi nữa ) 
 		closeBrowserAndDriver();
+		
 //		if (driver != null) {
 //			
 //			driver.quit();
