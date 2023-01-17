45dfc8d230d7d4e258069135158a127fd70ca122
Anh Nguyen <xuananh194@gmail.com>
2023-01-17 16:31:49 +0700

    [commit] [Topic_101_FW49_Data_Driven_Testing]

--- testdata/com/nopcomerce/data/UserData.java
+++ testdata/com/nopcomerce/data/UserData.java
@@ -13,7 +13,9 @@ public class UserData {
 	// nhiêu class bên trong com.nopcommerce.data
 	// -> chỉ cần tạo ra 1 class bên trong chứa nhiều subclass quản lý data test là
 	// đủ
-
+	
+	
+	
 	public class Login {
 		
 		List<String> listString = new ArrayList<String>();
@@ -23,16 +25,29 @@ public class UserData {
 
 		ArrayList<String> arrList = new ArrayList<String>();
 	}
+	
+	
+	
 
-	public class Register {
+	public static class Register {
 
+		public static final String FIRST_NAME = "Automation";
+		public static final String LAST_NAME = "FC";
+		public static final String EMAIL_ADDRESS = "automationfc";
+		public static final String PASSWORD = "123456";
+		public static final String DATE = "10";
+		public static final String MONTH = "8";
+		public static final String YEAR = "1999";
 	}
 
-	public class Address {
-
+	public static class MyAccount {
+		
+		public static final String ADDRESS = "...";
+		public static final String CITY = "...";
+		public static final String PHONE = "...";
 	}
 
-	public class Invoice {
-
+	public static class Invoice {
+		
 	}
 }
